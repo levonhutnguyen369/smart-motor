8deae4c8d2af75539e46dcf358a90408566b1148
levonhutnguyen369 <levonhutnguyen369@gmail.com>
2026-07-03 15:50:56 +0700

    cors update

--- src/main/java/backend/datn/config/SecurityConfig.java
+++ src/main/java/backend/datn/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // Cho phép truy cập các endpoint /api/auth/** mà không cần xác thực

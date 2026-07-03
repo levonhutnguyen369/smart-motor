40bcab05fa5e0a2d95f15191b98dac97c310cee4
levonhutnguyen369 <levonhutnguyen369@gmail.com>
2026-07-03 19:57:32 +0700

    update user profile

--- src/main/java/backend/datn/controller/UserController.java
+++ src/main/java/backend/datn/controller/UserController.java
@@ -29,4 +29,25 @@ public class UserController {
         }
         return ResponseEntity.ok(new ApiResponse<>(true, "User profile retrieved successfully", user));
     }
+
+    @PutMapping("/profile")
+    public ResponseEntity<ApiResponse<?>> updateProfile(
+            @RequestHeader("Authorization") String bearerToken,
+            @RequestBody User updatedUser
+    ) {
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            bearerToken = bearerToken.substring(7);
+        }
+        Long userId = jwtService.getUserIdFromToken(bearerToken);
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, "User not found", null));
+        }
+        user.setFullName(updatedUser.getFullName());
+        user.setPhone(updatedUser.getPhone());
+        user.setNameVehicle(updatedUser.getNameVehicle());
+        user.setLincensePlate(updatedUser.getLincensePlate());
+        userRepository.save(user);
+        return ResponseEntity.ok(new ApiResponse<>(true, "User profile updated successfully", user));
+    }
 }
